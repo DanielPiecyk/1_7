c73fc2ebe80e7f38b939e99a32c71643ccf769f9
danielpiecyk <100698293+DanielPiecyk@users.noreply.github.com>
2022-03-15 14:25:57 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    for(int i=0;i<7;i++)
+      {
+        
+        for(int j=1;j<i;j++){System.out.print("*");}
+        System.out.print("\n");
+      }
+    for(int i=0;i<7;i++)
+      {
+        
+        for(int j=1;j<i;j++){System.out.print("*");}
+        System.out.print("\n");
+      }
+   
+  }
+}
